c3532bc91cef6f6971faf79f50eb618f6a92a85c
Joe Aulner <joseph.aulner@aviture.us.com>
2017-01-18 18:30:40 -0600

    Initial commit with CH 1 example code

---
+++ src/javagames/render/HelloWorldApp.java
@@ -0,0 +1,50 @@
+package javagames.render;
+
+import java.awt.*;
+import javax.swing.*;
+import javagames.util.*;
+
+public class HelloWorldApp extends JFrame {
+    private FrameRate frameRate;
+
+    private HelloWorldApp() {
+        frameRate = new FrameRate();
+    }
+
+    private void createAndShowGUI() {
+        GamePanel gamePanel = new GamePanel();
+        gamePanel.setBackground(Color.BLACK);
+        gamePanel.setPreferredSize(new Dimension(320, 240));
+        getContentPane().add(gamePanel);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Hello World!");
+        pack();
+        frameRate.initialize();
+        setVisible(true);
+    }
+
+    private class GamePanel extends JPanel {
+        public void paint(Graphics g) {
+            super.paint(g);
+            onPaint(g);
+        }
+    }
+
+    private void onPaint(Graphics g) {
+        frameRate.calculate();
+        g.setColor(Color.WHITE);
+        g.drawString(frameRate.getFrameRate(), 30, 30);
+        repaint();
+    }
+
+    public static void main(String[] args) {
+        final HelloWorldApp app = new HelloWorldApp();
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                app.createAndShowGUI();
+            }
+        });
+    }
+}
