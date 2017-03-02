c1f162b62986ee264ee2cf03056b0c9aee9e3a76
Joe Aulner <joseph.aulner@aviture.us.com>
2017-03-01 18:24:48 -0600

    Override cursor

--- project_03/src/cannonball/CannonballCommand.java
+++ project_03/src/cannonball/CannonballCommand.java
@@ -2,8 +2,10 @@ package cannonball;
 
 import cannonball.util.Matrix3x3f;
 import cannonball.util.SimpleFramework;
+import cannonball.util.Vector2f;
 import cannonball.world.CannonballManager;
 import cannonball.world.CityManager;
+import cannonball.world.VectorObject;
 
 import java.awt.*;
 import java.awt.event.ComponentEvent;
@@ -15,9 +17,10 @@ public class CannonballCommand extends SimpleFramework {
     private CannonballManager cannonballManager;
     private CityManager cityManager;
     private Matrix3x3f viewport;
+    private VectorObject cursor;
     private boolean gameOver;
     private float score;
-    private float scoreMultiplier;
+    private float cityCount;
 
     public CannonballCommand() {
         appBackground = Color.WHITE;
@@ -34,7 +37,7 @@ public class CannonballCommand extends SimpleFramework {
 
         gameOver = true;
         score = 0;
-        scoreMultiplier = 1;
+        cityCount = 1;
     }
 
     @Override
@@ -43,6 +46,15 @@ public class CannonballCommand extends SimpleFramework {
         cannonballManager = CannonballManager.getInstance();
         cityManager = CityManager.getInstance();
         viewport = getViewportTransform();
+
+        cursor = new VectorObject(new Vector2f[]{
+                new Vector2f(-1, 0),
+                new Vector2f(1, 0),
+                new Vector2f(0, 0),
+                new Vector2f(0, 1),
+                new Vector2f(0, -1)
+        }, 0, 0, Color.RED, viewport);
+        cursor.setScale(0.5f, 0.5f);
     }
 
     @Override
@@ -54,7 +66,8 @@ public class CannonballCommand extends SimpleFramework {
     @Override
     protected void processInput(float delta) {
         super.processInput(delta);
-        if (keyboard.keyDownOnce(KeyEvent.VK_SPACE)) {
+        if (gameOver && keyboard.keyDownOnce(KeyEvent.VK_SPACE)) {
+            score = 0;
             gameOver = false;
             cannonballManager.initialize();
             cityManager.initialize();
@@ -67,6 +80,12 @@ public class CannonballCommand extends SimpleFramework {
     @Override
     protected void updateObjects(float delta) {
         super.updateObjects(delta);
+
+        Vector2f worldMousePos = getWorldMousePosition();
+        cursor.setLocation(worldMousePos.x, worldMousePos.y);
+        cursor.setViewport(viewport);
+        cursor.updateWorld();
+
         if (gameOver) {
             return;
         }
@@ -75,13 +94,12 @@ public class CannonballCommand extends SimpleFramework {
         cannonballManager.update(delta, viewport);
         cityManager.update(viewport);
 
-        int cityCount = cityManager.getCities().size();
+        cityCount = cityManager.getCities().size();
         if (cityCount == 0) {
             gameOver = true;
             cannonballManager.onGameOver();
         }
-        scoreMultiplier = cityCount;
-        score += 10 * delta * scoreMultiplier;
+        score += 10 * delta * this.cityCount;
         if (cannonballManager.getCannonballDestroyed()) {
             score += 50;
         }
@@ -89,25 +107,36 @@ public class CannonballCommand extends SimpleFramework {
 
     @Override
     protected void render(Graphics g) {
-        super.render(g);
+        overrideCursor();
+
         float windVelocity = cannonballManager.getWindVelocity();
-        String windDirection = windVelocity > 0 ? "East" : "West";
+        char windDirection = windVelocity > 0 ? 'E' : 'W';
 
         g.setColor(Color.BLACK);
         if (gameOver && score == 0) {
-            g.drawString("Press [SPACE] to start.", 20, 40);
+            g.drawString("Press [SPACE] to start.", 20, 20);
         } else if (gameOver) {
-            g.drawString("Game Over!", 20, 40);
-            g.drawString(String.format("Final Score: %,.0f", score), 20, 60);
+            g.drawString("Game Over!", 20, 20);
+            g.drawString(String.format("Final Score: %,.0f", score), 20, 40);
+            g.drawString("Press [SPACE] to restart.", 20, 60);
         } else {
-            g.drawString(String.format("Wind direction: %s", windDirection), 20, 40);
-            g.drawString(String.format("Wind velocity: %.2f", Math.abs(windVelocity)), 20, 60);
-            g.drawString(String.format("Score Multiplier: x%.0f", scoreMultiplier), 20, 80);
-            g.drawString(String.format("Score: %,.0f", score), 20, 100);
+            g.drawString("Defend the factories!", 20, 20);
+            g.drawString(String.format("Wind: %.1f%c", Math.abs(windVelocity), windDirection), 20, 40);
+            g.drawString(String.format("Multiplier: x%.0f", cityCount), 20, 60);
+            g.drawString(String.format("Score: %,.0f", score), 20, 80);
         }
 
         cannonballManager.render(g);
         cityManager.render(g);
+        cursor.render(g);
+    }
+
+    private void overrideCursor() {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Image image = tk.createImage("");
+        Point point = new Point(0, 0);
+        String name = "Empty Cursor";
+        setCursor(tk.createCustomCursor(image, point, name));
     }
 
     @Override
